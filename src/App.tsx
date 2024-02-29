825ece1d1f3c04c8c5a1a529e7e09dff17018392
juuuunny <102502542+juuuunny@users.noreply.github.com>
2024-03-01 04:41:24 +0900

    [feat] ScrollToTop 함수 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./hooks/ScrollToTop";
 import Landing from "./pages/landing/Landing";
 import ArchiveMain from "./pages/archive-main/ArchiveMain";
 import ArchivePopularVideos from "./pages/archive-popular-videos/ArchivePopularVidoes";
@@ -7,6 +8,7 @@ import ArchiveTotalVideos from "./pages/archive-total-videos/ArchiveTotalVidoes"
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/archive" element={<ArchiveMain />}></Route>
