f8f40a600cc02d3919048a9abe624ad47a442aff
minseo-hong <mshong1007@gmail.com>
2024-03-06 21:44:46 +0900

    [Design] 트렌드 분석 스크롤 시 상단 검색 UI 추가

--- src/pages/trend-analysis/TAHome.tsx
+++ src/pages/trend-analysis/TAHome.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import { useSearchParams } from 'react-router-dom';
 
 import SearchBar from '../../components/trend-analysis/search/SearchBar';
 import Dashboard from '../../components/trend-analysis/dashboard/Dashboard';
@@ -7,20 +9,67 @@ import KeywordDetailTrend from '../../components/trend-analysis/keyword-detail-t
 import RelatedKeyword from '../../components/trend-analysis/related-keyword/RelatedKeyword';
 import KeywordRank from '../../components/trend-analysis/keyword-rank/KeywordRank';
 import SNSContent from '../../components/trend-analysis/sns-content/root/SNSContent';
+import SearchTopFixed from '../../components/trend-analysis/search/SearchTopFixed';
 
 const TAHome = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keywordTrendRef = useRef<HTMLDivElement>(null);
+  const relatedTrendRef = useRef<HTMLDivElement>(null);
+  const snsTrendRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scrollToMission = searchParams.get('scroll_to');
+    if (scrollToMission) {
+      if (scrollToMission === 'keyword-trend' && keywordTrendRef.current) {
+        keywordTrendRef.current.scrollIntoView({ behavior: 'smooth' });
+        setSearchParams({}, { replace: true });
+      } else if (
+        scrollToMission === 'related-trend' &&
+        relatedTrendRef.current
+      ) {
+        relatedTrendRef.current.scrollIntoView({ behavior: 'smooth' });
+        setSearchParams({}, { replace: true });
+      } else if (scrollToMission === 'sns-trend' && snsTrendRef.current) {
+        snsTrendRef.current.scrollIntoView({ behavior: 'smooth' });
+        setSearchParams({}, { replace: true });
+      }
+    }
+  }, [searchParams]);
+
   return (
-    <main>
-      <InnerArea>
-        <SearchBar />
-        <Dashboard />
-        <KeywordTrend />
-        <KeywordDetailTrend />
-        <RelatedKeyword />
-        <KeywordRank />
-        <SNSContent />
-      </InnerArea>
-    </main>
+    <>
+      <SearchTopFixed />
+      <main>
+        <InnerArea>
+          <SearchBar />
+          <Dashboard />
+          <section
+            id="keyword-trend"
+            className="scroll-mt-[16rem]"
+            ref={keywordTrendRef}
+          >
+            <KeywordTrend />
+            <KeywordDetailTrend />
+          </section>
+          <section
+            id="related-trend"
+            className="scroll-mt-[16rem]"
+            ref={relatedTrendRef}
+          >
+            <RelatedKeyword />
+            <KeywordRank />
+          </section>
+          <section
+            id="sns-trend"
+            className="scroll-mt-[16rem]"
+            ref={snsTrendRef}
+          >
+            <SNSContent />
+          </section>
+        </InnerArea>
+      </main>
+    </>
   );
 };
 
